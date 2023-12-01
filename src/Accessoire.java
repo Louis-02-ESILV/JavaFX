2eef8fdd15c8698da564bdcdd230102b135a9df2
Ali Bennouna Louridi <alibennouna003@gmail.com>
2023-12-01 08:46:47 +0100

    Exo POO
    Structure de base du projet basée sur un exo de TD

---
+++ src/Accessoire.java
@@ -0,0 +1,9 @@
+public class Accessoire extends Produit{
+    public Accessoire(String nom, int prix, int nbex){
+        super(nom,prix,nbex);
+    }
+    @Override
+    public String toString(){
+        return this.toString();
+    }
+}
