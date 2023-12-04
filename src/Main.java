e6d7726a1395adeabe3c15bc3057a68e3e261124
Ali Bennouna Louridi <alibennouna003@gmail.com>
2023-12-04 17:15:49 +0100

    Update of DB values et manipulation d'objets
    On peut récuperer les données de la DB et les manipuler en tant que produits dans l'app.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,17 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.JDBCType;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         ProduitDAO produitDAO = new ProduitDAO();
+        List<Produit> produits=produitDAO.obtenirTousLesProduits();
+        System.out.println(produits.get(0).toString());
+        produits.get(0).Vendre(6);
+        produitDAO.mettreAJourProduit(produits.get(0));
+        System.out.println(produits.get(0).toString());
+        /*Initiliser des produits basiques :
         produitDAO.supprimerTousLesProduits();
         Vetement jean = new Vetement( "Jean", 29.99, 50, 38);
         produitDAO.ajouterProduit(jean);
@@ -13,5 +21,6 @@ public class Main {
         produitDAO.ajouterProduit(sandales);
         Accessoire sac = new Accessoire("Sac",50,2);
         produitDAO.ajouterProduit(sac);
+         */
     }
 }
--- src/ProduitDAO.java
+++ src/ProduitDAO.java
@@ -1,7 +1,6 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProduitDAO {
     private static final String URL = "jdbc:mysql://localhost:3306/store";
@@ -17,6 +16,52 @@ public class ProduitDAO {
             e.printStackTrace();
         }
     }
+    private Produit mapResultSetToProduit(ResultSet resultSet) throws SQLException {
+        int id = resultSet.getInt("id");
+        String type = resultSet.getString("type");
+        String nom = resultSet.getString("nom");
+        double prix = resultSet.getDouble("prix");
+        int stock = resultSet.getInt("stock");
+        int taille = resultSet.getInt("taille");
+        int pointure = resultSet.getInt("pointure");
+
+        Produit produit;
+
+        switch (type) {
+            case "Vetement":
+                produit = new Vetement(nom, prix, stock, taille);
+                break;
+            case "Chaussure":
+                produit = new Chaussure(nom, prix, stock, pointure);
+                break;
+            // Ajoutez d'autres cas pour d'autres types si nécessaire
+            default:
+                produit = new Produit(nom, prix, stock);
+                break;
+        }
+
+        produit.setId(id);
+        return produit;
+    }
+    public List<Produit> obtenirTousLesProduits() {
+        List<Produit> produits = new ArrayList<>();
+
+        try (Connection connexion = DriverManager.getConnection(URL, UTILISATEUR, MOT_DE_PASSE)) {
+            String requete = "SELECT * FROM produits";
+            try (PreparedStatement preparedStatement = connexion.prepareStatement(requete)) {
+                try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                    while (resultSet.next()) {
+                        Produit produit = mapResultSetToProduit(resultSet);
+                        produits.add(produit);
+                    }
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return produits;
+    }
+
     public void ajouterProduit(Produit produit) {
         try (Connection connexion = DriverManager.getConnection(URL, UTILISATEUR, MOT_DE_PASSE)) {
             String requete = "INSERT INTO produits (id, type, nom, prix, stock, taille, pointure) VALUES (?, ?, ?, ?, ?, ?, ?)";
@@ -46,5 +91,31 @@ public class ProduitDAO {
             e.printStackTrace();
         }
     }
+    public void mettreAJourProduit(Produit produit) {
+        try (Connection connexion = DriverManager.getConnection(URL, UTILISATEUR, MOT_DE_PASSE)) {
+            String requete = "UPDATE produits SET nom=?, prix=?, stock=?, taille=?, pointure=? WHERE id=?";
+            try (PreparedStatement preparedStatement = connexion.prepareStatement(requete)) {
+                preparedStatement.setString(1, produit.getNom());
+                preparedStatement.setDouble(2, produit.getPrix());
+                preparedStatement.setInt(3, produit.getNbex());
 
+                if (produit instanceof Vetement) {
+                    preparedStatement.setInt(4, ((Vetement) produit).getTaille());
+                    preparedStatement.setNull(5, java.sql.Types.INTEGER);
+                } else if (produit instanceof Chaussure) {
+                    preparedStatement.setNull(4, java.sql.Types.INTEGER);
+                    preparedStatement.setInt(5, ((Chaussure) produit).getPointure());
+                } else {
+                    preparedStatement.setNull(4, java.sql.Types.INTEGER);
+                    preparedStatement.setNull(5, java.sql.Types.INTEGER);
+                }
+
+                preparedStatement.setInt(6, produit.getId());
+
+                preparedStatement.executeUpdate();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
